d04dfe222edbe67faf8ede01acf936eeb499007e
maalik <ritik18406@iiitd.ac.in>
2019-10-06 06:08:01 +0000

    optional file

--- send_files.sh
+++ send_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this is just a optional file in case you want to send your files to other machines via ssh
-# I originally had 8 machines to work with
+# I originally had 8 machines to work with XD
 # Can also other options like google drive or maybe email, python server also works.
 
 # script for sending ddos.sh & main.sh to machines
