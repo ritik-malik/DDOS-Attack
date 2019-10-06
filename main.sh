06125a06a0e408b4c0466f6095e9d887b10cdfba
maalik <ritik18406@iiitd.ac.in>
2019-10-06 06:00:44 +0000

    main file

--- main.sh
+++ main.sh
@@ -2,15 +2,9 @@
 
 i=0
 
-
-
-# yes haha | ./test.sh   (to give input)
-
-
-while [ $i -lt 10000 ]; do
-
-nohup bash ddos.sh &
-i=$((i+1))
-echo yeah $i
+while [ $i -lt 10000 ]; do  # optimal value is 10000, cange it as per your requirement
+                            # I would say it might freeze above 10000 (atleast for me)
+  nohup bash ddos.sh &
+  i=$((i+1))
+  echo yeah $i  # will give the count for tracking
 done
-
