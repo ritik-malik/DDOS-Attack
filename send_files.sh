a9c4164e1e04262e2fd41c17cc0d90e717b58530
maalik <ritik18406@iiitd.ac.in>
2019-10-06 11:23:03 +0530

    Add files via upload

---
+++ send_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# script for sending ddos.sh & main.sh to machines
+
+#sshpass -p 'student' scp ddos.sh main.sh iiitd@192.168.32.216:~/Desktop
+sshpass -p 'student' scp ddos.sh main.sh iiitd@192.168.32.214:~/Desktop
+
