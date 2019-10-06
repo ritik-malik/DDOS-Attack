ae48c885126f95c3cf0f5a72731d5b31af3aa782
maalik <ritik18406@iiitd.ac.in>
2019-10-06 06:07:23 +0000

    optional file

--- test1.sh
+++ test1.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# just a test file to change permissions on a host machine via ssh
+
 sshpass -p 'student' ssh iiitd@192.168.32.215  'cd Desktop/;
+	chmod +x main.sh ddos.sh;
+	./main.sh;'
+sshpass -p 'student' ssh iiitd@192.168.32.213  'cd Desktop/;
+	chmod +x main.sh ddos.sh;
+	./main.sh;'
+sshpass -p 'student' ssh iiitd@192.168.32.214  'cd Desktop/;
+	chmod +x main.sh ddos.sh;
+	./main.sh;'
+sshpass -p 'student' ssh iiitd@192.168.32.216  'cd Desktop/;
 	chmod +x main.sh ddos.sh;
 	./main.sh;'
